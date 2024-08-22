45713305f49355b7010f197dbad1c4430faf63c0
Anish Shejawale <anishejawale@gmail.com>
2024-08-22 17:03:57 +0530

    Initial Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import ExplorePage from "./pages/ExplorePage";
 import LikesPage from "./pages/LikesPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
           <Route path="/explore" element={<ExplorePage />} />
           <Route path="/likes" element={<LikesPage />} />
         </Routes>
-        {/* <Toaster /> */}
+        <Toaster />
       </div>
     </div>
   );
